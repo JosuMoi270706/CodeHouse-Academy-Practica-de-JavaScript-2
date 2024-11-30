a82d3ed3b14f22362eb2f160a68ce58238d2eb91
angelchambi <angelchambi03@gmail.com>
2024-11-30 14:10:45 -0500

    ejer4_terminado

--- Eje4.js
+++ Eje4.js
@@ -8,12 +8,12 @@ let array1 = [15, 25, 35, 45, 55];
 let array2 = [60, 70, 80, 90, 100];
 
 // Función que concatena dos arrays
-function concatenarArrays(array1, array2) {
-    return array1.concat(array2);
+function concatenarArrays(array1, array2) {  // realizamos la funcion concatenar arrays del array 1 y array 2 
+    return array1.concat(array2); //retornamos el array 1 concatenado con el array 2
 }
 
 // Llamar a la función concatenarArrays con array1 y array2
-let arrayConcatenado = concatenarArrays(array1, array2);
+let arrayConcatenado = concatenarArrays(array1, array2); 
 
 // Mostrar el resultado en la consola
 console.log("Array concatenado:", arrayConcatenado);
